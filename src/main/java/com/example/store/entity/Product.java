82c4706cc7f0f2c67915ff23e467943cb44ec620
dimakzela <dimakzela@gmail.com>
2026-08-07 17:11:01 +0200

    fix spotless

--- src/main/java/com/example/store/entity/Product.java
+++ src/main/java/com/example/store/entity/Product.java
@@ -6,8 +6,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
-
 import jakarta.persistence.Table;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
